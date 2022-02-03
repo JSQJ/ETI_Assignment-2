5d0d7a0c00c379cccab65a8a2c22ce7a83c843ba
JSQJ <jeremysim891@gmail.com>
2022-02-03 11:42:09 +0800

    create,edit,view comments

--- Answers/main.go
+++ Answers/main.go
@@ -17,7 +17,8 @@ type answercomments struct {
 	Comment   string `json:"comment"`
 	StudentID string `json:"studentid"`
 }
-type answerratingss struct {
+type answerratings struct {
+	RatingID  string `json:"ratingid"`
 	AnswerID  string `json:"answerid"`
 	AnsRating int    `json:"answerrating"`
 }
@@ -53,23 +54,65 @@ func GetComments(db *sql.DB, AnswerID string) answercomments {
 			panic(err.Error())
 		}
 	}
-	fmt.Println(&comments.CommentID, &comments.AnswerID,
-		&comments.Comment, &comments.StudentID)
+	//fmt.Println(&comments.CommentID, &comments.AnswerID,
+		//&comments.Comment, &comments.StudentID)
 	return comments
 }
 
-func EditComment(db *sql.DB, driver driverinfo) bool {
-	if driver.DriverID == "" {
+func EditComment(db *sql.DB, comment answercomments) bool {
+	if comment.CommentID == "" {
 		return false
 	}
-	query := fmt.Sprintf("UPDATE Driver SET FirstName = '%s', LastName = '%s', MobileNumber= '%s', LicenseNumber = '%s', EmailAddress = '%s' WHERE DriverID = '%s';",
-		driver.FirstName, driver.LastName, driver.MobileNumber, driver.LicenseNumber, driver.EmailAddress, driver.DriverID)
+	query := fmt.Sprintf("UPDATE AnswerComments SET Comments = '%s' WHERE ID = '%d';",
+		comment.Comment,comment.CommentID)
 	_, err := db.Query(query)
 
 	if err != nil {
 		panic(err.Error())
 	}
 	return true
+}
+
+func CreateComments(db *sql.DB, Comment answercomments) bool {
+	query := fmt.Sprintf("INSERT INTO AnswerComments(ID, AnswerID, Comments, StudentID) VALUES (%d,'%s', '%s', '%s')",
+		getlastid(db)+1,
+		Comment.AnswerID,
+		Comment.Comment,
+		Comment.StudentID)
+
+	_, err := db.Query(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return true
+}
+
+func getlastid(db *sql.DB) int { //Gets the last id of passengers
+	query1 := "SELECT COUNT(*) FROM AnswerComments"
+	query2 := "SELECT ID FROM AnswerComments ORDER BY ID DESC LIMIT 1"
+	var commentCount int
+	results, err := db.Query(query1) //Run Query
+	if err != nil {
+		panic(err.Error())
+	}
+	if results.Next() {
+		results.Scan(&commentCount)
+	}
+	if commentCount > 0 {
+		results, err := db.Query(query2) //Run Query
+		var ID int
+		if err != nil {
+			panic(err.Error())
+		}
+		if results.Next() {
+			results.Scan(&ID)
+		}
+		return ID
+	} else {
+		return 0
+	}
+
 }
 
 func comments(w http.ResponseWriter, r *http.Request) {
@@ -86,16 +129,190 @@ func comments(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "DELETE" {
 		println("Can't delete comment")
 	} else if r.Method == "GET" {
-		//GET Driver using email address
+		//GET Comment using answer id
 		Comment := GetComments(db, params["AnswerID"])
 		if err != nil {
 			fmt.Printf("There was an error encoding the json. err = %s", err)
-		} else if DriverInformation.EmailAddress == "" { // Check if data is empty
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			w.Write([]byte("Driver does not exists"))
-			return
 		} else {
-			json.NewEncoder(w).Encode(GetDriver(db, DriverInformation.EmailAddress))
+			json.NewEncoder(w).Encode(GetComments(db, Comment.AnswerID))
+			w.WriteHeader(http.StatusAccepted)
+			return
+		}
+	}
+	if r.Header.Get("Content-type") == "application/json" {
+		if err != nil {
+			fmt.Println(err)
+		}
+		// POST is for creating new comment
+		if r.Method == "POST" {
+
+			var newComment answercomments
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				// convert JSON to object
+				json.Unmarshal(reqBody, &newComment)
+
+				if newComment.Comment == "" {
+					w.WriteHeader(
+						http.StatusUnprocessableEntity)
+					w.Write([]byte(
+						"422 - Please enter the required information " +
+							"in JSON format"))
+					return
+				} else {
+					CreateComments(db, newComment)
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("Comment created successfully"))
+					return
+				}
+			}
+		}
+		//---PUT is for creating or updating
+		// existing comments---
+		if r.Method == "PUT" {
+			var comment answercomments
+			reqBody, err := ioutil.ReadAll(r.Body)
+
+			if err == nil {
+				json.Unmarshal(reqBody, &comment)
+
+				if comment.CommentID == "" {
+					w.WriteHeader(
+						http.StatusUnprocessableEntity)
+					w.Write([]byte(
+						"422 - Please supply driver " +
+							" information " +
+							"in JSON format"))
+					return
+				} else {
+					//To update comment details
+					EditComment(db, comment)
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("comment updated successfully"))
+					return
+				}
+			}
+		}
+
+	}
+}
+
+func GetAnswersWithComments(db *sql.DB, AnswerID string) answercomments {
+	query := fmt.Sprintf("Select * FROM AnswerComments WHERE AnswerID= '%s' AND COUNT(", AnswerID)
+	results, err := db.Query(query)
+	if err != nil {
+		panic(err.Error())
+	}
+	var comments answercomments
+	for results.Next() {
+		// map this type to the record in the table
+		err = results.Scan(&comments.CommentID, &comments.AnswerID,
+			&comments.Comment, &comments.StudentID)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	fmt.Println(&comments.CommentID, &comments.AnswerID,
+		&comments.Comment, &comments.StudentID)
+	return comments
+}
+
+func GetRatings(db *sql.DB, AnswerID string) answerratings {
+	query := fmt.Sprintf("Select * FROM AnswerRatings WHERE AnswerID= '%s'", AnswerID)
+	results, err := db.Query(query)
+	if err != nil {
+		panic(err.Error())
+	}
+	var ratings answerratings
+	for results.Next() {
+		// map this type to the record in the table
+		err = results.Scan(&ratings.RatingID, &ratings.AnswerID,
+			&ratings.AnsRating)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	//fmt.Println(&ratings.RatingID, &ratings.AnswerID,
+		//&ratings.AnsRating)
+	return ratings
+}
+
+func EditRatings(db *sql.DB, ratings answerratings) bool {
+	if ratings.AnswerID == "" {
+		return false
+	}
+	query := fmt.Sprintf("UPDATE AnswerRatings SET Rating = '%d' WHERE ID = '%s';",
+	ratings.AnsRating)
+	_, err := db.Query(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return true
+}
+
+func CreateRatings(db *sql.DB, ratings answerratings) bool {
+	query := fmt.Sprintf("INSERT INTO AnswerRatings(ID, AnswerID, Rating) VALUES (%d,'%s',%d)",
+		getlastid(db)+1,
+		ratings.AnswerID,
+		ratings.AnsRating
+		)
+
+	_, err := db.Query(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return true
+}
+
+func getlastid(db *sql.DB) int { //Gets the last id of passengers
+	query1 := "SELECT COUNT(*) FROM AnswerComments"
+	query2 := "SELECT ID FROM AnswerComments ORDER BY ID DESC LIMIT 1"
+	var commentCount int
+	results, err := db.Query(query1) //Run Query
+	if err != nil {
+		panic(err.Error())
+	}
+	if results.Next() {
+		results.Scan(&commentCount)
+	}
+	if commentCount > 0 {
+		results, err := db.Query(query2) //Run Query
+		var ID int
+		if err != nil {
+			panic(err.Error())
+		}
+		if results.Next() {
+			results.Scan(&ID)
+		}
+		return ID
+	} else {
+		return 0
+	}
+
+}
+
+func ratings(w http.ResponseWriter, r *http.Request) {
+	if !validKey(r) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("401 - Invalid key"))
+		return
+	}
+	db, err := sql.Open("mysql", "root:12N28c02@tcp(127.0.0.1:3306)/assignment2_db") //connect to database
+	if err != nil {
+		fmt.Println(err)
+	}
+	params := mux.Vars(r)
+	if r.Method == "DELETE" {
+		println("Can't delete comment")
+	} else if r.Method == "GET" {
+		//GET Ratings using Answer ID
+		Comment := GetComments(db, params["AnswerID"])
+		if err != nil {
+			fmt.Printf("There was an error encoding the json. err = %s", err)
+		} else {
+			json.NewEncoder(w).Encode(GetComments(db, Comment.AnswerID))
 			w.WriteHeader(http.StatusAccepted)
 			return
 		}
@@ -107,13 +324,13 @@ func comments(w http.ResponseWriter, r *http.Request) {
 		// POST is for creating new course
 		if r.Method == "POST" {
 
-			var newDriver driverinfo
+			var newComment answercomments
 			reqBody, err := ioutil.ReadAll(r.Body)
 			if err == nil {
 				// convert JSON to object
-				json.Unmarshal(reqBody, &newDriver)
+				json.Unmarshal(reqBody, &newComment)
 
-				if newDriver.EmailAddress == "" {
+				if newComment.Comment == "" {
 					w.WriteHeader(
 						http.StatusUnprocessableEntity)
 					w.Write([]byte(
@@ -123,16 +340,10 @@ func comments(w http.ResponseWriter, r *http.Request) {
 				} else {
 					// check if driver already exists by email; add only if
 					// driver does not exist
-					if !CheckDriver(db, newDriver.EmailAddress) {
-						CreateDriver(db, newDriver)
-						w.WriteHeader(http.StatusCreated)
-						w.Write([]byte("Driver created successfully"))
-						return
-					} else {
-						w.WriteHeader(http.StatusUnprocessableEntity)
-						w.Write([]byte("Email is already in use"))
-						return
-					}
+					CreateComments(db, newComment)
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("Comment created successfully"))
+					return
 				}
 			}
 		}
@@ -141,13 +352,13 @@ func comments(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "PUT" {
 			//---PUT is for creating or updating
 			// existing driver---
-			var driver driverinfo
+			var comment answercomments
 			reqBody, err := ioutil.ReadAll(r.Body)
 
 			if err == nil {
-				json.Unmarshal(reqBody, &driver)
+				json.Unmarshal(reqBody, &comment)
 
-				if driver.EmailAddress == "" {
+				if comment.CommentID == "" {
 					w.WriteHeader(
 						http.StatusUnprocessableEntity)
 					w.Write([]byte(
@@ -158,21 +369,49 @@ func comments(w http.ResponseWriter, r *http.Request) {
 				} else {
 					// check if Driver does not exists; update only if
 					// driver does exist
-					if !CheckDriver(db, driver.EmailAddress) {
-						w.WriteHeader(http.StatusUnprocessableEntity)
-						w.Write([]byte("There is no exsiting driver with " + driver.EmailAddress))
-						return
-					} else {
-						//To update driver details
-						EditDriver(db, driver)
-						w.WriteHeader(http.StatusCreated)
-						w.Write([]byte("Driver updated successfully"))
-						return
-					}
-
+					//To update driver details
+					EditComment(db, comment)
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("comment updated successfully"))
+					return
 				}
 			}
 		}
 
+	}
+}
+func getCommentedAnswers(w http.ResponseWriter, r *http.Request) {
+	if !validKey(r) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("401 - Invalid key"))
+		return
+	}
+	db, err := sql.Open("mysql", "root:12N28c02@tcp(127.0.0.1:3306)/assignment2_db") //connect to database
+	if err != nil {
+		fmt.Println(err)
+	}
+	params := mux.Vars(r)
+	//GET is for getting comments
+	if r.Method == "GET" {
+		//GET comments using answerid
+		Comment := GetComments(db, params["AnswerID"])
+		if err != nil {
+			fmt.Printf("There was an error encoding the json. err = %s", err)
+		} else {
+			json.NewEncoder(w).Encode(GetComments(db, Comment.AnswerID))
+			w.WriteHeader(http.StatusAccepted)
+			return
+		}
+	}
+}
+
+func getRatedAnswers(w http.ResponseWriter, r *http.Request) {
+	//GET is for getting passenger trips
+	if r.Method == "GET" {
+		params := mux.Vars(r)
+		passengerID := params["passengerid"]
+		var passengerTrips []Trip = getTrips(passengerID)
+		json.NewEncoder(w).Encode(passengerTrips)
+		fmt.Println(passengerTrips)
 	}
 }
--- Questions/main.go
+++ Questions/main.go
@@ -10,12 +10,175 @@ import (
 
 type questioncomments struct {
 	CommentID string `json:"commentid"`
-	AnswerID string `json:"answerid"`
 	QuestionID  string `json:"questionid"`
 	Comment   string `json:"comment"`
 	StudentID string `json:"studentid"`
 }
 type questionratingss struct {
-	AnswerID  string `json:"answerid"`
-	AnsRating int    `json:"answerrating"`
+	QuestionID  string `json:"answerid"`
+	QuestionRating int    `json:"answerrating"`
+}
+
+func GetComments(db *sql.DB, AnswerID string) answercomments {
+	query := fmt.Sprintf("Select * FROM AnswerComments WHERE AnswerID= '%s'", AnswerID)
+	results, err := db.Query(query)
+	if err != nil {
+		panic(err.Error())
+	}
+	var comments answercomments
+	for results.Next() {
+		// map this type to the record in the table
+		err = results.Scan(&comments.CommentID, &comments.AnswerID,
+			&comments.Comment, &comments.StudentID)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	fmt.Println(&comments.CommentID, &comments.AnswerID,
+		&comments.Comment, &comments.StudentID)
+	return comments
+}
+
+func EditComment(db *sql.DB, comment answercomments) bool {
+	if comment.CommentID == "" {
+		return false
+	}
+	query := fmt.Sprintf("UPDATE AnswerComments SET Comments = '%s' WHERE ID = '%s';",
+		comment.Comment)
+	_, err := db.Query(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return true
+}
+
+func CreateComments(db *sql.DB, Comment answercomments) bool {
+	query := fmt.Sprintf("INSERT INTO AnswerComments(ID, AnswerID, Comments, StudentID) VALUES (%d,'%s', '%s', '%s')",
+		getlastid(db)+1,
+		Comment.AnswerID,
+		Comment.Comment,
+		Comment.StudentID)
+
+	_, err := db.Query(query)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return true
+}
+
+func getlastid(db *sql.DB) int { //Gets the last id of passengers
+	query1 := "SELECT COUNT(*) FROM AnswerComments"
+	query2 := "SELECT ID FROM AnswerComments ORDER BY ID DESC LIMIT 1"
+	var commentCount int
+	results, err := db.Query(query1) //Run Query
+	if err != nil {
+		panic(err.Error())
+	}
+	if results.Next() {
+		results.Scan(&commentCount)
+	}
+	if commentCount > 0 {
+		results, err := db.Query(query2) //Run Query
+		var ID int
+		if err != nil {
+			panic(err.Error())
+		}
+		if results.Next() {
+			results.Scan(&ID)
+		}
+		return ID
+	} else {
+		return 0
+	}
+
+}
+
+func comments(w http.ResponseWriter, r *http.Request) {
+	if !validKey(r) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("401 - Invalid key"))
+		return
+	}
+	db, err := sql.Open("mysql", "root:12N28c02@tcp(127.0.0.1:3306)/assignment2_db") //connect to database
+	if err != nil {
+		fmt.Println(err)
+	}
+	params := mux.Vars(r)
+	if r.Method == "DELETE" {
+		println("Can't delete comment")
+	} else if r.Method == "GET" {
+		//GET Driver using email address
+		Comment := GetComments(db, params["AnswerID"])
+		if err != nil {
+			fmt.Printf("There was an error encoding the json. err = %s", err)
+		} else {
+			json.NewEncoder(w).Encode(GetComments(db, Comment.AnswerID))
+			w.WriteHeader(http.StatusAccepted)
+			return
+		}
+	}
+	if r.Header.Get("Content-type") == "application/json" {
+		if err != nil {
+			fmt.Println(err)
+		}
+		// POST is for creating new course
+		if r.Method == "POST" {
+
+			var newComment answercomments
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				// convert JSON to object
+				json.Unmarshal(reqBody, &newComment)
+
+				if newComment.Comment == "" {
+					w.WriteHeader(
+						http.StatusUnprocessableEntity)
+					w.Write([]byte(
+						"422 - Please enter the required information " +
+							"in JSON format"))
+					return
+				} else {
+					// check if driver already exists by email; add only if
+					// driver does not exist
+					CreateComments(db, newComment)
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("Comment created successfully"))
+					return
+				}
+			}
+		}
+		//---PUT is for creating or updating
+		// existing course---
+		if r.Method == "PUT" {
+			//---PUT is for creating or updating
+			// existing driver---
+			var comment answercomments
+			reqBody, err := ioutil.ReadAll(r.Body)
+
+			if err == nil {
+				json.Unmarshal(reqBody, &comment)
+
+				if comment.CommentID == "" {
+					w.WriteHeader(
+						http.StatusUnprocessableEntity)
+					w.Write([]byte(
+						"422 - Please supply driver " +
+							" information " +
+							"in JSON format"))
+					return
+				} else {
+					// check if Driver does not exists; update only if
+					// driver does exist
+					//To update driver details
+					EditComment(db, comment)
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("comment updated successfully"))
+					return
+				}
+			}
+		}
+
+	}
 }
